29f57fa12f1adec31391a69e123a800d9b51d579
jsvamberg <j.svamberg@seznam.cz>
2020-11-06 08:21:28 +0100

    Edit Linearizovaný model
    Edit opravení & dokončení linearizovaného modelu

--- Zdrojové kódy/Model_kyvadla_na_voziku_m.m
+++ Zdrojové kódy/Model_kyvadla_na_voziku_m.m
@@ -2,16 +2,6 @@ clear all;
 clc;
 close all;
 
-f = 0; %sila pusobici na vozik
-M = 50; %hmostnost voziku
-m = 5; %hmotnost tělesa na lane
-dx = 0; %pocatecni rychlost voziku
-x = 0; %pocatecni poloh voziku
-dphi = 0; %pocatecni rychlost kyvadla
-phi = 45*pi/180; %pocatecni poloha kyvadla ve °
-l = 1; %delka zavesu
-g = 9.81; %gravitacni sila
-
 %%
 syms ddphi_s ddx_s dx dphi x phi M m l f g
 
@@ -30,4 +20,110 @@ ddphi = simplifyFraction(ddphi)
 %  
 % ddphi_v = (g*phi + (l*m*phi*dphi^2 + f - m*(ddx + g*phi))/(M + m))/l
 
+%%
+syms x1 x2 x3 x4 u m g M l 
 
+f1 = x3
+f2 = x4
+f3 = (-m*g*sin(x2)*cos(x2)+m*x4^2*sin(x2)+u)/(M+m+m*(cos(x2))^2)
+f4 = (m*l*x4^2*sin(x2)*cos(x2)+u*cos(x2)+m*g*sin(x2)+M*g*sin(x2))/(l*(m+M+m*(cos(x2))^2))
+
+df1_dx1 = diff(f1, x1);
+df1_dx2 = diff(f1, x2);
+df1_dx3 = diff(f1, x3);
+df1_dx4 = diff(f1, x4);
+
+df2_dx1 = diff(f2, x1);
+df2_dx2 = diff(f2, x2);
+df2_dx3 = diff(f2, x3);
+df2_dx4 = diff(f2, x4);
+
+df3_dx1 = diff(f3, x1);
+df3_dx2 = diff(f3, x2);
+df3_dx3 = diff(f3, x3);
+df3_dx4 = diff(f3, x4);
+
+df4_dx1 = diff(f4, x1);
+df4_dx2 = diff(f4, x2);
+df4_dx3 = diff(f4, x3);
+df4_dx4 = diff(f4, x4);
+
+df1_du = diff(f1, u);
+df2_du = diff(f2, u);
+df3_du = diff(f3, u);
+df4_du = diff(f4, u);
+
+A = [df1_dx1 df1_dx2 df1_dx3 df1_dx4;
+    df2_dx1 df2_dx2 df2_dx3 df2_dx4;
+    df3_dx1 df3_dx2 df3_dx3 df3_dx4;
+    df4_dx1 df4_dx2 df4_dx3 df4_dx4]
+
+B = [df1_du;
+    df2_du;
+    df3_du;
+    df4_du]
+
+%%
+df1_dx1 = subs(df1_dx1, [x1 x2 x3 x4], [0 0 0 0]);
+df1_dx2 = subs(df1_dx2, [x1 x2 x3 x4], [0 0 0 0]);
+df1_dx3 = subs(df1_dx3, [x1 x2 x3 x4], [0 0 0 0]);
+df1_dx4 = subs(df1_dx4, [x1 x2 x3 x4], [0 0 0 0]);
+
+df2_dx1 = subs(df2_dx1, [x1 x2 x3 x4], [0 0 0 0]);
+df2_dx2 = subs(df2_dx2, [x1 x2 x3 x4], [0 0 0 0]);
+df2_dx3 = subs(df2_dx3, [x1 x2 x3 x4], [0 0 0 0]);
+df2_dx4 = subs(df2_dx4, [x1 x2 x3 x4], [0 0 0 0]);
+
+df3_dx1 = subs(df3_dx1, [x1 x2 x3 x4], [0 0 0 0]);
+df3_dx2 = subs(df3_dx2, [x1 x2 x3 x4], [0 0 0 0]);
+df3_dx3 = subs(df3_dx3, [x1 x2 x3 x4], [0 0 0 0]);
+df3_dx4 = subs(df3_dx4, [x1 x2 x3 x4], [0 0 0 0]);
+
+df4_dx1 = subs(df4_dx1, [x1 x2 x3 x4], [0 0 0 0]);
+df4_dx2 = subs(df4_dx2, [x1 x2 x3 x4], [0 0 0 0]);
+df4_dx3 = subs(df4_dx3, [x1 x2 x3 x4], [0 0 0 0]);
+df4_dx4 = subs(df4_dx4, [x1 x2 x3 x4], [0 0 0 0]);
+
+df1_du = subs(df1_du, [x1 x2 x3 x4], [0 0 0 0]);
+df2_du = subs(df2_du, [x1 x2 x3 x4], [0 0 0 0]);
+df3_du = subs(df3_du, [x1 x2 x3 x4], [0 0 0 0]);
+df4_du = subs(df4_du, [x1 x2 x3 x4], [0 0 0 0]);
+
+A = [df1_dx1 df1_dx2 df1_dx3 df1_dx4;
+    df2_dx1 df2_dx2 df2_dx3 df2_dx4;
+    df3_dx1 df3_dx2 df3_dx3 df3_dx4;
+    df4_dx1 df4_dx2 df4_dx3 df4_dx4]
+
+B = [df1_du;
+    df2_du;
+    df3_du;
+    df4_du]
+%%
+f = 0; %sila pusobici na vozik
+M = 50; %hmostnost voziku
+m = 5; %hmotnost tělesa na lane
+dx = 0; %pocatecni rychlost voziku
+x = 0; %pocatecni poloh voziku
+dphi = 0; %pocatecni rychlost kyvadla
+phi = 0; %pocatecni poloha kyvadla ve °
+l = 1; %delka zavesu
+g = 9.81; %gravitacni sila
+
+A = [0 0 1 0;
+    0 0 0 1;
+    0 -(g*m)/(M + 2*m) 0 0;
+    0 (M*g + g*m)/(l*(M + 2*m)) 0 0]
+B = [0;
+    0;
+    1/(M + 2*m);
+    1/(l*(M + 2*m))]
+C = [1 0 0 0;
+    0 1 0 0;
+    0 0 1 0;
+    0 0 0 1]
+D = 0
+
+sys = ss(A, B, C, D)
+
+tf(ss(A, B, C, D))
+%step(sys)
