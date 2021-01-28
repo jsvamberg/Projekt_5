4a7991035bc24146833a2bc24df629157595b05b
jsvamberg <j.svamberg@seznam.cz>
2021-01-28 11:45:05 +0100

    Add stavový regulátor

--- Zdrojové kódy/Model_kyvadla_na_voziku_m.m
+++ Zdrojové kódy/Model_kyvadla_na_voziku_m.m
@@ -20,12 +20,12 @@ close all;
 % dL_ddphi = diff(L, dphi)
 % dL_dphi = diff(L, phi)
 % 
-%%
-D_dL_ddx = M*ddx+m*ddx+m*ddphi*l*cos(phi)-m*dphi*l*sin(phi)
-D_dL_ddphi = ddphi*l^2*m*(sin(phi))^2+dphi*l^2*m*cos(phi)*2*sin(phi)
-
-eq1 = D_dL_ddx-dL_dx == f
-eq2 = D_dL_ddphi-dL_dphi == 0
+% %%
+% D_dL_ddx = M*ddx+m*ddx+m*ddphi*l*cos(phi)-m*dphi*l*sin(phi)
+% D_dL_ddphi = ddphi*l^2*m*(sin(phi))^2+dphi*l^2*m*cos(phi)*2*sin(phi)
+% 
+% eq1 = D_dL_ddx-dL_dx == f
+% eq2 = D_dL_ddphi-dL_dphi == 0
 
 %% Dosazení jedné rovnice za druhou
 clear;
@@ -81,37 +81,37 @@ B = [df1_du;
     df3_du;
     df4_du]
 
-%%
-clear;
-close;
-u = 0; %sila pusobici na vozik
-M = 15; %hmostnost voziku
-m = 5; %hmotnost tělesa na lane
-y3 = 0; %pocatecni rychlost voziku
-y1 = 0; %pocatecni poloh voziku
-y4 = 0; %pocatecni rychlost kyvadla
-y2 = 0*pi/180; %pocatecni poloha kyvadla ve °
-l = 1; %delka zavesu
-g = 9.81; %gravitacni sila
-
-A = [0, 0, 1, 0;
-    0, 0, 0, 1;
-    0, (g*m*sin(y2)^2 - g*m*cos(y2)^2 + m*y3*cos(y2)^2 - m*y3*sin(y2)^2 + m*y3*y4*sin(y2)^2 + l*m*y4^2*cos(y2) - m*y3*y4*cos(y2)^2)/(m*cos(y2)^2 + M + m) + (2*m*cos(y2)*sin(y2)*(l*m*sin(y2)*y4^2 - m*y3*cos(y2)*sin(y2)*y4 + u + m*y3*cos(y2)*sin(y2) - g*m*cos(y2)*sin(y2)))/(m*cos(y2)^2 + M + m)^2, (m*cos(y2)*sin(y2) - m*y4*cos(y2)*sin(y2))/(m*cos(y2)^2 + M + m), -(m*y3*cos(y2)*sin(y2) - 2*l*m*y4*sin(y2))/(m*cos(y2)^2 + M + m);
-    0, (g*m*cos(y2) - M*y3*cos(y2) - u*sin(y2) - m*y3*cos(y2) + M*g*cos(y2) + l*m*y4^2*cos(y2)^2 - l*m*y4^2*sin(y2)^2 + M*y3*y4*cos(y2) + m*y3*y4*cos(y2))/(l*(m*cos(y2)^2 + M + m)) + (2*m*cos(y2)*sin(y2)*(u*cos(y2) - M*y3*sin(y2) + g*m*sin(y2) - m*y3*sin(y2) + M*g*sin(y2) + M*y3*y4*sin(y2) + m*y3*y4*sin(y2) + l*m*y4^2*cos(y2)*sin(y2)))/(l*(m*cos(y2)^2 + M + m)^2), -(M*sin(y2) + m*sin(y2) - M*y4*sin(y2) - m*y4*sin(y2))/(l*(m*cos(y2)^2 + M + m)), (M*y3*sin(y2) + m*y3*sin(y2) + 2*l*m*y4*cos(y2)*sin(y2))/(l*(m*cos(y2)^2 + M + m))]
-
-B = [0;
-    0;
-    1/(m*cos(y2)^2 + M + m);
-    cos(y2)/(l*(m*cos(y2)^2 + M + m))]
-
-C = [1 0 0 0;
-    0 1 0 0;
-    0 0 1 0;
-    0 0 0 1]
-
-D = 0
-
-sys = ss(A, B, C, D)
+% %%
+% clear;
+% close;
+% u = 0; %sila pusobici na vozik
+% M = 15; %hmostnost voziku
+% m = 5; %hmotnost tělesa na lane
+% y3 = 0; %pocatecni rychlost voziku
+% y1 = 0; %pocatecni poloh voziku
+% y4 = 0; %pocatecni rychlost kyvadla
+% y2 = 0*pi/180; %pocatecni poloha kyvadla ve °
+% l = 1; %delka zavesu
+% g = 9.81; %gravitacni sila
+% 
+% A = [0, 0, 1, 0;
+%     0, 0, 0, 1;
+%     0, (g*m*sin(y2)^2 - g*m*cos(y2)^2 + m*y3*cos(y2)^2 - m*y3*sin(y2)^2 + m*y3*y4*sin(y2)^2 + l*m*y4^2*cos(y2) - m*y3*y4*cos(y2)^2)/(m*cos(y2)^2 + M + m) + (2*m*cos(y2)*sin(y2)*(l*m*sin(y2)*y4^2 - m*y3*cos(y2)*sin(y2)*y4 + u + m*y3*cos(y2)*sin(y2) - g*m*cos(y2)*sin(y2)))/(m*cos(y2)^2 + M + m)^2, (m*cos(y2)*sin(y2) - m*y4*cos(y2)*sin(y2))/(m*cos(y2)^2 + M + m), -(m*y3*cos(y2)*sin(y2) - 2*l*m*y4*sin(y2))/(m*cos(y2)^2 + M + m);
+%     0, (g*m*cos(y2) - M*y3*cos(y2) - u*sin(y2) - m*y3*cos(y2) + M*g*cos(y2) + l*m*y4^2*cos(y2)^2 - l*m*y4^2*sin(y2)^2 + M*y3*y4*cos(y2) + m*y3*y4*cos(y2))/(l*(m*cos(y2)^2 + M + m)) + (2*m*cos(y2)*sin(y2)*(u*cos(y2) - M*y3*sin(y2) + g*m*sin(y2) - m*y3*sin(y2) + M*g*sin(y2) + M*y3*y4*sin(y2) + m*y3*y4*sin(y2) + l*m*y4^2*cos(y2)*sin(y2)))/(l*(m*cos(y2)^2 + M + m)^2), -(M*sin(y2) + m*sin(y2) - M*y4*sin(y2) - m*y4*sin(y2))/(l*(m*cos(y2)^2 + M + m)), (M*y3*sin(y2) + m*y3*sin(y2) + 2*l*m*y4*cos(y2)*sin(y2))/(l*(m*cos(y2)^2 + M + m))]
+% 
+% B = [0;
+%     0;
+%     1/(m*cos(y2)^2 + M + m);
+%     cos(y2)/(l*(m*cos(y2)^2 + M + m))]
+% 
+% C = [1 0 0 0;
+%     0 1 0 0;
+%     0 0 1 0;
+%     0 0 0 1]
+% 
+% D = 0
+% 
+% sys = ss(A, B, C, D)
 %% linearizace v bode [0, 0, 0, 0]
 df1_dy1 = subs(df1_dy1, [y1 y2 y3 y4], [0 pi 0 0]);
 df1_dy2 = subs(df1_dy2, [y1 y2 y3 y4], [0 pi 0 0]);
@@ -151,13 +151,13 @@ B = [df1_du;
 %% Konkretizace hodnot a zjisteni konkretniho modelu a prenosovych funkci
 clear;
 close;
-f = 10; %sila pusobici na vozik
+f = 0; %sila pusobici na vozik
 M = 15; %hmostnost voziku
 m = 5; %hmotnost tělesa na lane
 dx = 0; %pocatecni rychlost voziku
 x = 0; %pocatecni poloh voziku
 dphi = 0; %pocatecni rychlost kyvadla
-phi = 5*pi/180; %pocatecni poloha kyvadla ve °
+phi = 0*pi/180; %pocatecni poloha kyvadla ve °
 l = 1; %delka zavesu
 g = 9.81; %gravitacni sila
 
@@ -174,12 +174,28 @@ C = [1 0 0 0;
     0 1 0 0;
     0 0 1 0;
     0 0 0 1]
-D = 0
+D = [0;
+    0;
+    0;
+    0]
 
 sys = ss(A, B, C, D)
 
+% Riditelnost
+rank(ctrb(sys.A, sys.B)) %system je riditelny, protoze ma plnou radkovou hodnost
+
+
 tf = tf(ss(A, B, C, D))
 
+%% Navrh stavoveho regulatoru
+
+p1 = -10 + 0i
+p2 = -10 + 0i
+p3 = -10 + 2.8014i
+p4 = -10 - 2.8014i
+
+K = acker(sys.A, sys.B, [p1, p2, p3, p4])
+sys_cl = ss(sys.A-sys.B*K,sys.B,sys.C,sys.D)
 %%
 out=sim('Model_kyvadla_na_voziku_m_slx.slx',50)
 
@@ -235,4 +251,28 @@ title('Nelinearizovany model - Uhlova rychlost kyvadla')
 xlabel('t [s]')
 ylabel('\omega [rad/s]')
 
-%%
+figure;
+subplot(4,1,1)
+plot(out.stav_x);
+legend('x')
+title('Linearni model se stav. reg. - Poloha vozíku')
+xlabel('t [s]')
+ylabel('s [m]')
+subplot(4,1,2)
+plot(out.stav_phi);
+legend('\phi')
+title('Linearni model se stav. reg. - Uhel kyvadla \phi')
+xlabel('t [s]')
+ylabel('\phi [°]')
+subplot(4,1,3)
+plot(out.stav_dx);
+legend('dx')
+title('Linearni model se stav. reg. - Rychlost voziku')
+xlabel('t [s]')
+ylabel('v [m/s]')
+subplot(4,1,4)
+plot(out.stav_dphi);
+legend('d\phi')
+title('Linearni model se stav. reg. - Uhlova rychlost kyvadla')
+xlabel('t [s]')
+ylabel('\omega [rad/s]')
